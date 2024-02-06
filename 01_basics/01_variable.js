60159491d9b8ff3594d8894592887429e1fb9281
Shubham Singh <134995991+shubhamrkt@users.noreply.github.com>
2024-02-06 10:11:29 +0000

    config

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId=1245
+let accountEmail="hyz@mail.com"
+var accountPassword="123456"
+accountCity="jaipur"
+let accountHouse;
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountHouse])
